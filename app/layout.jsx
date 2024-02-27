e02d3ef76e5f401595924b9a992e9df0c6f87cbb
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-27 20:29:56 +0100

    Render Footer component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@/assets/styles/globals.css'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export const metadata = {
     title: 'Rental Radar | Find The Perfect Rental',
@@ -14,6 +15,7 @@ const MainLayout = ({children}) => {
             <body>
                 <Navbar />
                 <main>{children}</main>
+                <Footer />
             </body>
         </html>
     )
