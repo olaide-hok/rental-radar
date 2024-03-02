5113c1c304f163502f840e42df71fb0c242c2ba4
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-02 10:46:54 +0100

    Create fetchProperty utility function

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
+// Fetch All properties
 async function fetchProperties() {
     try {
         // Handle the case where the Domain is not available yet.
@@ -19,4 +20,24 @@ async function fetchProperties() {
     }
 }
 
-export {fetchProperties}
+// Fetch single property
+async function fetchProperty(id) {
+    try {
+        // Handle the case where the Domain is not available yet.
+        if (!apiDomain) {
+            return null
+        }
+        const res = await fetch(`${apiDomain}/properties/${id}`)
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch data')
+        }
+
+        return res.json()
+    } catch (error) {
+        console.error(error)
+        return null
+    }
+}
+
+export {fetchProperties, fetchProperty}
