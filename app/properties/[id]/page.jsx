85f210e433a40defc50b50ec76b3ac6758326743
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-05 20:36:02 +0100

    Add and render PropertyImages component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import PropertyDetails from '@/components/PropertyDetails'
 import Spinner from '@/components/Spinner'
 import {FaArrowLeft} from 'react-icons/fa'
+import PropertyImages from '@/components/PropertyImages'
 
 const PropertyPage = () => {
     const {id} = useParams()
@@ -144,6 +145,7 @@ const PropertyPage = () => {
                             </div>
                         </div>
                     </section>
+                    <PropertyImages images={property.images} />
                 </>
             )}
         </>
