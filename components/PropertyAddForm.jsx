423583957d4b550e21e7a1d189eb762fe2308967
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-05 15:01:29 +0100

    Handle form data by adding action, method, encType attribute to form.
    Add required attribute to image input element.
    
    Remove period from seller_name name input attribute.

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -100,7 +100,10 @@ const PropertyAddForm = () => {
     }, [])
     return (
         mounted && (
-            <form>
+            <form
+                action="/api/properties"
+                method="POST"
+                encType="multipart/form-data">
                 <h2 className="text-3xl text-center font-semibold mb-6">
                     Add Property
                 </h2>
@@ -545,7 +548,7 @@ const PropertyAddForm = () => {
                     <input
                         type="text"
                         id="seller_name"
-                        name="seller_info.name."
+                        name="seller_info.name"
                         className="border rounded w-full py-2 px-3"
                         placeholder="Name"
                         value={fields.seller_info.name}
@@ -599,6 +602,7 @@ const PropertyAddForm = () => {
                         className="border rounded w-full py-2 px-3"
                         accept="image/*"
                         multiple
+                        required
                         onChange={handleImageChange}
                     />
                 </div>
