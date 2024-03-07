d22be93d32f6422cd8653660f5cab9f503f693b4
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-07 21:04:59 +0100

    Update fetchProperties Fn

--- utils/requests.js
+++ utils/requests.js
@@ -1,13 +1,16 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
 // Fetch All properties
-async function fetchProperties() {
+async function fetchProperties({showFeatured = false} = {}) {
     try {
         // Handle the case where the Domain is not available yet.
         if (!apiDomain) {
             return []
         }
-        const res = await fetch(`${apiDomain}/properties`, {cache: 'no-store'})
+        const res = await fetch(
+            `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+            {cache: 'no-store'}
+        )
 
         if (!res.ok) {
             throw new Error('Failed to fetch data')
