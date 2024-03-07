47ed12de4171af63318e78ef3073cfa0dbc40ab0
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-07 21:17:58 +0100

    Update import name

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import FeaturedProperties from '@/components/FeaturedProperties'
 import Hero from '@/components/Hero'
 import HomeProperties from '@/components/HomeProperties'
-import InfoBoxes from '@/components/infoBoxes'
+import InfoBoxes from '@/components/InfoBoxes'
 import React from 'react'
 
 const HomePage = () => {
