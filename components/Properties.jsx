45991e072f76da1732d202d5ce9267aa5bdcfb45
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-07 20:26:44 +0100

    Render Pagination component and pass in props

--- components/Properties.jsx
+++ components/Properties.jsx
@@ -2,19 +2,20 @@
 import {useEffect, useState} from 'react'
 import PropertyCard from './PropertyCard'
 import Spinner from './Spinner'
+import Pagination from './Pagination'
 
 const Properties = () => {
     const [properties, setProperties] = useState([])
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
-    const [pageSize, setPageSize] = useState(3)
+    const [pageSize, setPageSize] = useState(6)
     const [totalItems, setTotalItems] = useState(0)
 
     useEffect(() => {
         const fetchProperties = async () => {
             try {
                 const res = await fetch(
-                    `/api/properties?$page=${page}&pageSize=${pageSize}`
+                    `/api/properties?page=${page}&pageSize=${pageSize}`
                 )
 
                 if (!res.ok) {
@@ -31,7 +32,11 @@ const Properties = () => {
             }
         }
         fetchProperties()
-    }, [])
+    }, [page, pageSize])
+
+    const handlePageChange = (newPage) => {
+        setPage(newPage)
+    }
 
     return loading ? (
         <Spinner />
@@ -50,6 +55,12 @@ const Properties = () => {
                         ))}
                     </div>
                 )}
+                <Pagination
+                    page={page}
+                    pageSize={pageSize}
+                    totalItems={totalItems}
+                    onPageChange={handlePageChange}
+                />
             </div>
         </section>
     )
